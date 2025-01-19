1a51ec8476033c323fdb0d4b2c3b71c3fe1c1cdf
i-madhav <madhav.shar06ma@gmail.com>
2025-01-19 18:38:52 +0530

    webhooks

--- apps/web/app/api/webhooks/route.ts
+++ apps/web/app/api/webhooks/route.ts
@@ -1,29 +1,37 @@
-import { Webhook } from 'svix';
-import { headers } from 'next/headers';
-import { WebhookEvent } from '@clerk/nextjs/server';
-import { PrismaClient } from '@prisma/client';
+import { Webhook } from 'svix'
+import { headers } from 'next/headers'
+import { WebhookEvent } from '@clerk/nextjs/server'
 
-export async function POST(req: Request){
-  const prisma = new PrismaClient();
-  const SIGNING_SECRET = process.env.SIGNING_SECRET;
+export async function POST(req: Request) {
+  const SIGNING_SECRET = process.env.SIGNING_SECRET
 
   if (!SIGNING_SECRET) {
-    throw new Error('Error: Please add SIGNING_SECRET from Clerk Dashboard to .env or .env.local');
+    throw new Error('Error: Please add SIGNING_SECRET from Clerk Dashboard to .env or .env.local')
   }
 
-  const wh = new Webhook(SIGNING_SECRET);
-  const headerPayload = await headers();
-  const svix_id = headerPayload.get('svix-id');
-  const svix_timestamp = headerPayload.get('svix-timestamp');
-  const svix_signature = headerPayload.get('svix-signature');
-  if (!svix_id || !svix_timestamp || !svix_signature){
-    return new Response('Error: Missing Svix headers', {status: 400});
+  // Create new Svix instance with secret
+  const wh = new Webhook(SIGNING_SECRET)
+
+  // Get headers
+  const headerPayload = await headers()
+  const svix_id = headerPayload.get('svix-id')
+  const svix_timestamp = headerPayload.get('svix-timestamp')
+  const svix_signature = headerPayload.get('svix-signature')
+
+  // If there are no headers, error out
+  if (!svix_id || !svix_timestamp || !svix_signature) {
+    return new Response('Error: Missing Svix headers', {
+      status: 400,
+    })
   }
 
-  const payload = await req.json();
-  const body = JSON.stringify(payload);
+  // Get body
+  const payload = await req.json()
+  const body = JSON.stringify(payload)
 
   let evt: WebhookEvent
+
+  // Verify payload with headers
   try {
     evt = wh.verify(body, {
       'svix-id': svix_id,
@@ -32,17 +40,17 @@ export async function POST(req: Request){
     }) as WebhookEvent
   } catch (err) {
     console.error('Error: Could not verify webhook:', err)
-    return new Response('Error: Verification error',{status: 400})
+    return new Response('Error: Verification error', {
+      status: 400,
+    })
   }
 
-  //const { id } = evt.data;
-  const eventType = evt.type;
-  if(eventType === "user.created"){
-    const response = await prisma.user.create({data:{clerkUserId:evt.data.id,email:evt.data.email_addresses[0]!.email_address}})
-    if(response){
-        console.log("User is created successfully");
-        console.log(response);
-    }}
+  // Do something with payload
+  // For this guide, log payload to console
+  const { id } = evt.data
+  const eventType = evt.type
+  console.log(`Received webhook with ID ${id} and event type of ${eventType}`)
+  console.log('Webhook payload:', body)
 
-  return new Response('Webhook received', { status: 200 });
+  return new Response('Webhook received', { status: 200 })
 }
