ba873a7e5500227a15074cc4046d962c448bcdad
i-madhav <madhav.shar06ma@gmail.com>
2025-01-19 15:51:30 +0530

    changes

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,4 +1,19 @@
 import { nextJsConfig } from "@repo/eslint-config/next-js";
 
 /** @type {import("eslint").Linter.Config} */
-export default nextJsConfig;
+const config = {
+  ...nextJsConfig,
+  rules: {
+    // Set all rules to "off"
+    "no-console": "off",
+    "no-alert": "off",
+    // Add any other specific rules you want to disable here
+  },
+};
+
+// Disable all rules by setting them to "off"
+Object.keys(config.rules).forEach(rule => {
+  config.rules[rule] = "off";
+});
+
+export default config;
