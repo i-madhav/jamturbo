98961c8cfbab877486ddadb444aeefe60e0c9bca
i-madhav <madhav.shar06ma@gmail.com>
2025-01-24 09:16:08 +0530

    changes server

--- apps/server/src/index.js
+++ apps/server/src/index.js
@@ -1,17 +1,22 @@
-import http from "http";
+import express from "express";
+import { createServer } from "http";
 import { Server } from "socket.io";
 import handleSocketConnection from "./service/socket.js";
 
-const httpServer = http.createServer();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const httpServer = createServer(app);
 const io = new Server(httpServer, {
-    cors: {
-        origin: "*"
-    }
-})
+  cors: {
+    origin: "*",
+  },
+});
 
-httpServer.listen(8000, () => {
-    console.log(`Server is running  on 8000 port`);
-})
+handleSocketConnection(io);
 
-handleSocketConnection();
-export {io};
+httpServer.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+export { io };
