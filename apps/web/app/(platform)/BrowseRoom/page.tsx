3d636687ac78e1bb7b3f41622377502ddb9e8b6c
i-madhav <madhav.shar06ma@gmail.com>
2025-01-24 09:34:35 +0530

    fix bug

--- apps/web/app/(platform)/BrowseRoom/page.tsx
+++ apps/web/app/(platform)/BrowseRoom/page.tsx
@@ -90,7 +90,7 @@ const BrowsePublicRoompage = () => {
         </p>
       </header>
 
-      {publicRooms.length > 0 ? (
+      {publicRooms?.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
           {publicRooms.map((room) => (
             <Link
